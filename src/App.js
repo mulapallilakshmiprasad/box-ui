22c2705dab0d193a5a35c507bf79e122ebc02d99
manojkumardesai <manojkumardbe@gmail.com>
2020-07-08 16:27:32 +0530

    add dependencies to package

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { ContentExplorer } from 'box-ui-elements';
+import {IntlProvider} from 'react-intl';
 
 function App() {
   return (
     <div className="App">
+      <IntlProvider locale="en">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ContentExplorer
+        fileId={0}
+        token={'bZLtlgnUia4v39zXJUoxpU9zjyYJAjzz'}
+        />
       </header>
+      </IntlProvider>
     </div>
   );
 }
